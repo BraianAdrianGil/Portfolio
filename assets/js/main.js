3deb28a0c640a3a408faa2ee0b124ea0700232b1
BraianAdrianGil <brai_06_95@outlook.com>
2023-02-10 20:48:53 -0300

    changed nav__menu rule

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const navToggle= document.querySelector(".nav__toggle")
 const navMenu= document.querySelector(".nav__menu")
 navToggle.addEventListener("click", () =>{
-    navMenu.classList.toggle("nav__menu__visible")
+    navMenu.classList.toggle("nav__menu__not__visible")
 })
 // ============================================
 const experience=document.querySelector(".experience")
