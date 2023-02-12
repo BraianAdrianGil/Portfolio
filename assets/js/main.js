b78f189f2a0735512cab8a55b71bd00f30f298de
BraianAdrianGil <brai_06_95@outlook.com>
2023-02-12 19:14:14 -0300

    solved problem with css rule nav__menu__fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,9 +20,10 @@ removeVisible(contact)
 //==============================================
 window.addEventListener("scroll", () =>{
     if(window.scrollY> 0){
-        navMenu.classList.add("nav__menu__fixed")
         navToggle.classList.add("nav__toggle__fixed")
+        navMenu.classList.add("nav__menu__fixed")
     } else {
         navToggle.classList.remove("nav__toggle__fixed")
+        navMenu.classList.remove("nav__menu__fixed")
     }
 })
